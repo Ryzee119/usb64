f8125249b16715f65bf9c39e128ac83193da7995
Ryzee119 <wendland@live.com.au>
2020-07-22 14:21:06 +0930

    Update msc_disk.c

--- src/usbd/msc_disk.c
+++ src/usbd/msc_disk.c
@@ -2,7 +2,7 @@
  * The MIT License (MIT)
  *
  * Copyright (c) 2019 Ha Thach (tinyusb.org)
- * Copyright (c) 2019 Ryan Wendland (USB64)
+ * Copyright (c) 2020 Ryan Wendland (Modified for USB64)
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
