6be9858cb9645806eb959e27939b640cb95952ce
Ryzee119 <wendland@live.com.au>
2020-09-21 08:17:59 +0930

    Update n64_wrapper.h

--- src/teensy41/n64_wrapper.h
+++ src/teensy41/n64_wrapper.h
@@ -34,15 +34,21 @@ extern "C" {
 #define N64_OUTPUT 1
 #define N64_INPUT 2
 
+//RTC wrapper prototypes (For gameboy roms with RTC, i.e Pokemon games)
 void n64hal_rtc_read(uint16_t *day, uint8_t *h, uint8_t *m, uint8_t *s, uint32_t *dst);
 void n64hal_rtc_write(uint16_t *day, uint8_t *h, uint8_t *m, uint8_t *s, uint32_t *dst);
+  
+//Timer wrappers
 uint32_t n64hal_hs_tick_get_speed();
 void n64hal_hs_tick_init();
 void n64hal_hs_tick_reset();
 uint32_t n64hal_hs_tick_get();
-void n64hal_ram_read(void *rx_buff, void *src, uint32_t len);
-void n64hal_ram_write(void *tx_buff, void *dst, uint32_t len);
+  
+//RAM access wrappers
+void n64hal_ram_read(void *rx_buff, void *src, uint32_t offset, uint32_t len);
+void n64hal_ram_write(void *tx_buff, void *dst, uint32_t offset, uint32_t len);
 
+//GPIO wrappers
 void n64hal_input_swap(n64_controller *controller, uint8_t val);
 uint8_t n64hal_input_read(n64_controller *controller);
 
