5fb68a74c4be413c810208f722b2d6e26985a116
Ryzee119 <wendland@live.com.au>
2021-10-26 13:56:25 +1030

    Main: Make all main code cpp files

--- src/fileio.h
+++ src/fileio.h
@@ -4,10 +4,6 @@
 #ifndef _FILEIO_H
 #define _FILEIO_H
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 #include <Arduino.h>
 #include "usb64_conf.h"
 
@@ -19,9 +15,5 @@ uint32_t fileio_list_directory(char **list, uint32_t max);
 int fileio_open_file_readonly(const char *filename);
 void fileio_close_file();
 int fileio_get_line(char* buffer, int max_len);
-
-#ifdef __cplusplus
-}
-#endif
 
 #endif
