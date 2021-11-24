ae4e3b747e10c72119ab1f00324ffaf8df7454d2
Ryzee119 <wendland@live.com.au>
2021-11-24 19:17:28 +1030

    FileIO: Add status check function

--- src/fileio.h
+++ src/fileio.h
@@ -6,11 +6,14 @@
 
 #include "usb64_conf.h"
 
+//File access API
 void fileio_init(void);
 void fileio_write_to_file(char *filename, uint8_t *data, uint32_t len);
 void fileio_read_from_file(char *filename, uint32_t file_offset, uint8_t *data, uint32_t len);
 uint32_t fileio_list_directory(char **list, uint32_t max);
+bool fileio_detected();
 
+//Device specific API. Implement for porting.
 bool fileio_dev_init();
 int fileio_dev_open_dir(const char* dir);
 void fileio_dev_close_dir(int handle);
