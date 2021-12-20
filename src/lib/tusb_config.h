3f7254c88950496edc81a4b4a6c3c59fabc230ce
Ryzee119 <wendland@live.com.au>
2021-12-21 09:06:16 +1030

    tuh: Allow multiple hubs

--- src/lib/tusb_config.h
+++ src/lib/tusb_config.h
@@ -74,7 +74,7 @@
 // Size of buffer to hold descriptors and other data used for enumeration
 #define CFG_TUH_ENUMERATION_BUFSIZE 256
 
-#define CFG_TUH_HUB                 1
+#define CFG_TUH_HUB                 4
 #define CFG_TUH_CDC                 0
 #define CFG_TUH_HID                 4 // typical keyboard + mouse device can have 3-4 HID interfaces
 #define CFG_TUH_MSC                 0
