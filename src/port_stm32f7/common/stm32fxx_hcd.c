f426c8d6d95ece90767a547d0d79256e4488251c
Ryzee119 <wendland@live.com.au>
2022-01-09 16:25:03 +1030

    stm32f7 usbh: Share control pipe, refactor functions

--- src/port_stm32f7/common/stm32fxx_hcd.c
+++ src/port_stm32f7/common/stm32fxx_hcd.c
@@ -2,6 +2,7 @@
  * The MIT License (MIT)
  *
  * Copyright (c) 2018, hathach (tinyusb.org)
+ * Copyright (c) 2022, Ryzee119
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -31,18 +32,26 @@
 #if CFG_TUSB_MCU == OPT_MCU_STM32F7
 #include "device/dcd.h"
 
-//Have active pipes. STM32F7 is 12 for FS and 16 for HS
+static HCD_HandleTypeDef hhcd;
+static bool root_port_connected = 0;
+
+//STM32F7 is 12 for FS and 16 for HS
 #define MAX_PIPES 12
 #ifndef MAX_ENDPOINTS
-#define MAX_ENDPOINTS 16
+#define MAX_ENDPOINTS 32
 #endif
+
 //The amount of shared channels reserved for interrupt transfer queue. If you are polling many interrupt endpoints
 //increasing this can increase the overall responsiveness.
-//This must be < MAX_PIPES, and must allow for free pipes for control, bulk and iso transfers
+//This must be < MAX_PIPES, and MAX_ENDPOINTS - INTR_CHANNELS must allow for free pipes for control, bulk and iso transfers
 #ifndef INTR_CHANNELS
-#define INTR_CHANNELS 1
+#define INTR_CHANNELS 6
 #endif
-static uint8_t intr_lock[INTR_CHANNELS] = {0}; //Make sure we dont use a pipe thats already in use.
+//Single shared control channel just after the reserved interrupt channels
+#define CTRL_CHANNEL INTR_CHANNELS
+
+//Locks to make sure we dont use a interrupt pipe thats already in use.
+static uint8_t intr_lock[INTR_CHANNELS] = {0};
 
 typedef struct
 {
@@ -55,14 +64,10 @@ typedef struct
     tusb_desc_endpoint_t ep_desc;
     uint8_t *xfer_buff;
     uint32_t xfer_len;
-    uint8_t toggle_in;
-    uint8_t toggle_out;
+    uint8_t toggle;
 } usbh_endpoint_t;
 
 usbh_endpoint_t usbh_endpoint[MAX_ENDPOINTS] = {0};
-
-static HCD_HandleTypeDef hhcd;
-static bool root_port_connected = 0;
 
 static usbh_endpoint_t *_alloc_endpoint(uint8_t dev_addr, tusb_desc_endpoint_t *ep_desc)
 {
@@ -96,14 +101,19 @@ static usbh_endpoint_t *_alloc_endpoint(uint8_t dev_addr, tusb_desc_endpoint_t *
             {
                 usbh_endpoint[i].ch_num = 0;
             }
+            else if (usbh_endpoint[i].ep_type == EP_TYPE_CTRL)
+            {
+                usbh_endpoint[i].ch_num = CTRL_CHANNEL;
+            }
+            //Bulk and iso are allocated their own channel, allocated after interrupt and control channels
             else
             {
-                //First 2 channels are reserved for interrupt endpoints (in and out)
-                for (int ch_num = INTR_CHANNELS; ch_num < MAX_PIPES; ch_num++)
+                for (int ch_num = CTRL_CHANNEL; ch_num < MAX_PIPES; ch_num++)
                 {
                     if (hhcd.hc[ch_num].max_packet == 0)
                     {
                         usbh_endpoint[i].ch_num = ch_num;
+                        hhcd.hc[ch_num].max_packet = 8; //Set to a standard non-zero number.
                         break;
                     }
                 }
@@ -114,6 +124,7 @@ static usbh_endpoint_t *_alloc_endpoint(uint8_t dev_addr, tusb_desc_endpoint_t *
             }
             else
             {
+                TU_LOG1("ERROR: No more host channels!\n");
                 break;
             }
         }
@@ -123,11 +134,6 @@ static usbh_endpoint_t *_alloc_endpoint(uint8_t dev_addr, tusb_desc_endpoint_t *
 
 static usbh_endpoint_t *_find_endpoint(uint8_t dev_addr, uint8_t ep_addr)
 {
-    //If it's a control pipe, remove direction bit. Use same endpoint for IN/OUT
-    if (ep_addr == 0x80)
-    {
-        ep_addr = 0x00;
-    }
     for (int i = 0; i < MAX_ENDPOINTS; i++)
     {
         if (usbh_endpoint[i].allocated == false)
@@ -142,10 +148,45 @@ static usbh_endpoint_t *_find_endpoint(uint8_t dev_addr, uint8_t ep_addr)
     return NULL;
 }
 
+static bool _endpoint_xfer(usbh_endpoint_t *ep, uint8_t is_setup)
+{
+    HAL_StatusTypeDef ret = HAL_OK;
+    HCD_HCTypeDef *hc = &hhcd.hc[ep->ch_num];
+    uint8_t hc_ep_addr = hc->ep_num | ((hc->ep_is_in) ? 0x80 : 0x00);
+    if (hc->dev_addr != ep->dev_addr || hc_ep_addr != ep->ep_desc.bEndpointAddress || hc->dev_addr == 0)
+    {
+        ret = HAL_HCD_HC_Init(&hhcd,
+                              ep->ch_num,
+                              ep->ep_desc.bEndpointAddress,
+                              ep->dev_addr,
+                              HAL_HCD_GetCurrentSpeed(&hhcd), //FIXME tuh_speed_get?
+                              ep->ep_type,
+                              ep->ep_desc.wMaxPacketSize);
+    }
+
+    uint8_t *toggle = (hc->ep_is_in) ? &hc->toggle_in : &hc->toggle_out;
+    *toggle = ep->toggle;
+
+    ep->active = 1;
+    ep->last_frame = HAL_HCD_GetCurrentFrame(&hhcd);
+
+    if (ret == HAL_OK)
+    {
+        ret = HAL_HCD_HC_SubmitRequest(&hhcd,
+                                       ep->ch_num,
+                                       (tu_edpt_dir(ep->ep_desc.bEndpointAddress) == TUSB_DIR_IN) ? 1 : 0,
+                                       ep->ep_type,
+                                       (is_setup) ? 0 : 1,
+                                       (uint8_t *)ep->xfer_buff,
+                                       ep->xfer_len,
+                                       0);
+    }
+    return (ret == HAL_OK);
+}
+
 static void deferred_resetport(void *param)
 {
     HAL_HCD_ResetPort(&hhcd);
-    //Device is reset, alert TinyUSB.
     hcd_event_device_attach(0, false);
 }
 
@@ -165,7 +206,6 @@ void HAL_HCD_Connect_Callback(HCD_HandleTypeDef *hhcd)
 
 void HAL_HCD_Disconnect_Callback(HCD_HandleTypeDef *hhcd)
 {
-    TU_LOG3("%s\n", __FUNCTION__);
     root_port_connected = 0;
     hcd_event_device_remove(0, true);
 }
@@ -181,56 +221,24 @@ void HAL_HCD_SOF_Callback(HCD_HandleTypeDef *hhcd)
         {
             return;
         }
+
         uint8_t ep_index = i % MAX_ENDPOINTS;
-        if (!usbh_endpoint[ep_index].active)
+        usbh_endpoint_t *ep = &usbh_endpoint[ep_index];
+
+        if (ep->active == 0)
         {
             continue;
         }
-
-        usbh_endpoint_t *ep = &usbh_endpoint[ep_index];
 
         if (ep->ep_type == EP_TYPE_INTR && intr_lock[intr_channel] == 0)
         {
             if ((HAL_HCD_GetCurrentFrame(hhcd) - ep->last_frame) >= ep->ep_desc.bInterval)
             {
-                //Get the currently active channel and store the toggle values
-                HCD_HCTypeDef *hc = &hhcd->hc[intr_channel];
-                uint8_t old_ep_addr = hc->ep_num | ((hc->ep_is_in) ? 0x80 : 0x00);
-                usbh_endpoint_t *old_ep = _find_endpoint(hc->dev_addr, old_ep_addr);
-                if (old_ep != NULL)
-                {
-                    old_ep->toggle_in = hc->toggle_in;
-                    old_ep->toggle_out = hc->toggle_out;
-                }
-                else
-                {
-                    TU_LOG1("ERROR, could not find ep\n");
-                }
-
-                //TU_LOG1("inter %d\n", ep->ep_desc.bInterval);
                 ep->ch_num = intr_channel;
-                HAL_HCD_HC_Init(hhcd,
-                                ep->ch_num,
-                                ep->ep_desc.bEndpointAddress,
-                                ep->dev_addr,
-                                HAL_HCD_GetCurrentSpeed(hhcd), //FIXME tuh_speed_get?
-                                ep->ep_type,
-                                ep->ep_desc.wMaxPacketSize);
-                hc->toggle_in = ep->toggle_in;
-                hc->toggle_out = ep->toggle_out;
-                HAL_HCD_HC_SubmitRequest(hhcd,
-                                         ep->ch_num,
-                                         (tu_edpt_dir(ep->ep_desc.bEndpointAddress) == TUSB_DIR_IN) ? 1 : 0,
-                                         ep->ep_type,
-                                         1,
-                                         ep->xfer_buff,
-                                         ep->xfer_len,
-                                         0);
-                ep->last_frame = HAL_HCD_GetCurrentFrame(hhcd);
+                _endpoint_xfer(ep, 0);
                 intr_in_queue_head = (i + 1) % MAX_ENDPOINTS;
                 intr_lock[intr_channel] = ep->dev_addr;
                 intr_channel++;
-                //TU_LOG1("tx: %02x %d\n", ep->dev_addr, ep->toggle_in);
             }
         }
     }
@@ -246,29 +254,41 @@ void HAL_HCD_HC_NotifyURBChange_Callback(HCD_HandleTypeDef *hhcd, uint8_t ch_num
     //Interrupt transfer is done, stop the transfer. Wait for user callback to reschedule
     if (hc->ep_type == EP_TYPE_INTR)
     {
-        if ((urb_state == URB_DONE || urb_state == URB_STALL || urb_state == URB_ERROR))
-        {
-            ep->active = 0;
-        }
         intr_lock[ch_num] = 0;
     }
 
     if (urb_state == URB_DONE)
     {
-        TU_LOG3("Transfer SUCCESS devadr%02x ep:%02x %d bytes\n", hc->dev_addr, ep_addr, actual_length);
+        TU_LOG2("Transfer SUCCESS devaddr %02x ep:%02x %d bytes, channel %d\n", hc->dev_addr, ep_addr, actual_length, ch_num);
+        ep->toggle = (hc->ep_is_in) ? hc->toggle_in ^ 1 : hc->toggle_out;
+        ep->active = 0;
         hcd_event_xfer_complete(hc->dev_addr, ep_addr, actual_length, XFER_RESULT_SUCCESS, true);
     }
     else if (urb_state == URB_STALL)
     {
-        TU_LOG1("Transfer ERROR: URB_STALL, state %02x dev addr: %02x\n", hc->state, hc->dev_addr);
+        TU_LOG1("Transfer ERROR: URB_STALL, state %02x dev addr: %02x channel %d\n", hc->state, hc->dev_addr, ch_num);
         hc->state = HC_IDLE;
+        ep->active = 0;
         hcd_event_xfer_complete(hc->dev_addr, ep_addr, actual_length, XFER_RESULT_STALLED, true);
     }
-    else if (urb_state == URB_ERROR)
+    else if (urb_state == URB_ERROR || hc->state == HC_XACTERR)
     {
-        TU_LOG1("Transfer ERROR: URB_ERROR, state %02x dev addr: %02x\n", hc->state, hc->dev_addr);
+        TU_LOG1("Transfer ERROR: URB_ERROR, state %02x dev addr: %02x channel %d\n", hc->state, hc->dev_addr, ch_num);
         hc->state = HC_IDLE;
+        ep->active = 0;
         hcd_event_xfer_complete(hc->dev_addr, ep_addr, actual_length, XFER_RESULT_FAILED, true);
+    }
+    //Automatically retry output NAK.
+    else if (urb_state == URB_NOTREADY && hc->state == HC_NAK && hc->ep_is_in == 0)
+    {
+        //Retry output NAK. FIXME? Retry count? Only for control endpoint?
+        uint32_t tmpreg;
+        USB_OTG_GlobalTypeDef *USBx = hhcd->Instance;
+        uint32_t USBx_BASE = (uint32_t)USBx;
+        tmpreg = USBx_HC(ch_num)->HCCHAR;
+        tmpreg &= ~USB_OTG_HCCHAR_CHDIS;
+        tmpreg |= USB_OTG_HCCHAR_CHENA;
+        USBx_HC(ch_num)->HCCHAR = tmpreg;
     }
 }
 
@@ -281,7 +301,7 @@ void hcd_device_close(uint8_t rhport, uint8_t dev_addr)
         usbh_endpoint_t *ep = &usbh_endpoint[i];
         if (ep->allocated == true && ep->dev_addr == dev_addr)
         {
-            TU_LOG1("Closing pipe %d on dev %02x\n", i, dev_addr);
+            TU_LOG2("Closing pipe %d on dev %02x\n", i, dev_addr);
             if (hhcd.hc[ep->ch_num].dev_addr == ep->dev_addr)
             {
                 tu_memclr(&hhcd.hc[ep->ch_num], sizeof(HCD_HCTypeDef));
@@ -377,78 +397,62 @@ bool hcd_init(uint8_t rhport)
 bool hcd_edpt_open(uint8_t rhport, uint8_t dev_addr, tusb_desc_endpoint_t const *ep_desc)
 {
     (void)rhport;
-    TU_LOG3("%s\n", __FUNCTION__);
-    int ret = HAL_OK;
 
-    usbh_endpoint_t *new_ep = _alloc_endpoint(dev_addr, (tusb_desc_endpoint_t *)ep_desc);
-    TU_ASSERT(new_ep != NULL);
-    if (new_ep->ep_type != EP_TYPE_INTR)
+    usbh_endpoint_t *ep = _alloc_endpoint(dev_addr, (tusb_desc_endpoint_t *)ep_desc);
+    TU_ASSERT(ep != NULL);
+    TU_LOG2("Opened pipe for devaddr:%d epaddr:%02x in slot %d\n", dev_addr, ep->ep_desc.bEndpointAddress, ep->ch_num);
+    if (ep_desc->bEndpointAddress == 0x00)
     {
-        //For non interrupt endpoints, I allocate a pipe in the host controller immediately.
-        tu_memclr(&hhcd.hc[new_ep->ch_num], sizeof(HCD_HCTypeDef));
-        TU_ASSERT(ep_desc->wMaxPacketSize > 0); //FIXME?, I rely on mps > 0 for pipe allocation
-        ret = HAL_HCD_HC_Init(&hhcd,
-                              new_ep->ch_num,
-                              new_ep->ep_desc.bEndpointAddress,
-                              new_ep->dev_addr,
-                              HAL_HCD_GetCurrentSpeed(&hhcd), //FIXME tuh_speed_get?
-                              new_ep->ep_type,
-                              new_ep->ep_desc.wMaxPacketSize);
+        //If control endpoint, also opening IN pipe
+        ep = _alloc_endpoint(dev_addr, (tusb_desc_endpoint_t *)ep_desc);
+        ep->ep_desc.bEndpointAddress |= 0x80;
+        TU_ASSERT(ep != NULL);
+        TU_LOG2("Opened pipe for devaddr:%d epaddr:%02x in slot %d\n", dev_addr, ep->ep_desc.bEndpointAddress, ep->ch_num);
     }
-    TU_LOG1("Opened pipe for devaddr:%d epaddr:%02x in slot %d\n", dev_addr, new_ep->ep_desc.bEndpointAddress, new_ep->ch_num);
-    return (ret == HAL_OK);
+    return true;
 }
 
 bool hcd_setup_send(uint8_t rhport, uint8_t dev_addr, uint8_t const setup_packet[8])
 {
     (void)rhport;
-    TU_LOG3("%s\n", __FUNCTION__);
+
     usbh_endpoint_t *ep = _find_endpoint(dev_addr, 0x00);
-    HAL_StatusTypeDef ret = HAL_HCD_HC_SubmitRequest(&hhcd,
-                                                     ep->ch_num,
-                                                     0,
-                                                     EP_TYPE_CTRL,
-                                                     0,
-                                                     (uint8_t *)setup_packet,
-                                                     8,
-                                                     0);
-    return (ret == HAL_OK);
+    TU_ASSERT(ep != NULL);
+
+    ep->xfer_buff = (uint8_t *)setup_packet;
+    ep->xfer_len = 8;
+
+    TU_LOG2("hcd_setup_send on pipe for devaddr:%d in slot %d, ep_type %d, len %d\n",
+            dev_addr, ep->ch_num, ep->ep_type, 8);
+
+    return _endpoint_xfer(ep, 1);
 }
 
 bool hcd_edpt_xfer(uint8_t rhport, uint8_t dev_addr, uint8_t ep_addr, uint8_t *buffer, uint16_t buflen)
 {
     (void)rhport;
-    HAL_StatusTypeDef ret = HAL_OK;
+    bool ret = true;
     usbh_endpoint_t *ep = _find_endpoint(dev_addr, ep_addr);
     TU_ASSERT(ep != NULL);
 
-    ep->active = 1;
     ep->xfer_buff = buffer;
     ep->xfer_len = buflen;
 
+    TU_LOG2("hcd_edpt_xfer on pipe for devaddr:%d epaddr:%02x in slot %d, ep_type %d, ep_dir %d, len %d\n",
+            ep->dev_addr, ep->ep_desc.bEndpointAddress, ep->ch_num, ep->ep_type, tu_edpt_dir(ep_addr), buflen);
+
+    //Non interrupt endpoints, queue immediately.
     if (ep->ep_type != EP_TYPE_INTR)
     {
-        HCD_HCTypeDef *hc = &hhcd.hc[ep->ch_num];
-        if (hc->urb_state == URB_NOTREADY || hc->urb_state == URB_NYET ||
-            (hc->state != HC_IDLE && hc->state != HC_XFRC))
-        {
-            TU_LOG1("Error: Endpoint %02x is busy, urb: %02x hc: %02x\n", ep_addr, hc->urb_state, hc->state);
-            TU_ASSERT(0);
-        }
-        ret = HAL_HCD_HC_SubmitRequest(&hhcd,
-                                       ep->ch_num,
-                                       (tu_edpt_dir(ep_addr) == TUSB_DIR_IN) ? 1 : 0,
-                                       ep->ep_type,
-                                       1,
-                                       buffer,
-                                       buflen,
-                                       0);
+        ret = _endpoint_xfer(ep, 0);
+    }
+    //Otherwise, mark it as after and it will be schedule in SOF interrupt
+    else
+    {
+        ep->active = 1;
     }
 
-    TU_LOG2("hcd_edpt_xfer on pipe for devaddr:%d epaddr:%02x in slot %d, ep_type %d, ep_dir %d, len %d\n",
-            dev_addr, ep_addr, ep->ch_num, ep->ep_type, tu_edpt_dir(ep_addr), buflen);
-
-    return (ret == HAL_OK);
+    return ret;
 }
 
 void hcd_int_handler(uint8_t rhport)
